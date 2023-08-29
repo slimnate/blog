0a27957e4e94e2a2fb0a201f239a446b54cf2bfe
SlimNate <slimnatekc@gmail.com>
2023-08-29 00:45:47 -0500

    update git_include liquid tag to infer language from file extension, and allow for lang specification as second argument

--- _plugins/git-include.rb
+++ _plugins/git-include.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 require 'rouge'
 
 module Jekyll
+
     class RenderGithubTag < Liquid::Tag
         GITHUB_HOST_RAW  = 'raw.githubusercontent.com'
 
@@ -12,13 +13,32 @@ module Jekyll
             @text = text
         end
 
+        def infer_lang(url)
+            extension = url.rpartition('.').last
+            if(extension == 'cpp' || extension == 'h')
+                return 'cpp'
+            else
+                return extension
+            end
+        end
+
         def render(context)
-            uri = URI(@text.strip)
+            # Get params from invocation
+            params = @text.strip.split
+            # first param is url
+            uri = URI(params[0])
+            # second param is lang
+            lang = params[1]
+            # if no lang provided, try to detect from url from url
+            if lang.nil?
+                lang = infer_lang(params[0])
+            end
+
             uri.host = GITHUB_HOST_RAW
             uri.path = uri.path.sub('/blob', '')
             code_lines = uri.open.readlines
 
-            "```cpp\r\n#{code_lines.join}\r\n```"
+            "```#{lang}\r\n#{code_lines.join}\r\n```"
         end
     end
 end
